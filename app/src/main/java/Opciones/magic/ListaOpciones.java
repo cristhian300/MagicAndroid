5d072de04d488b4d150aa12882f7ac4b00294e09
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2020-10-12 11:30:19 -0500

    Menu Drawer Agregado Principal

---
+++ app/src/main/java/Opciones/magic/ListaOpciones.java
@@ -0,0 +1,15 @@
+package Opciones.magic;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.magicandroid.R;
+
+public class ListaOpciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_opciones);
+    }
+}
